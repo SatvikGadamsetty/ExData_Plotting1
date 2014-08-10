e5bc127df7e8aede3601d1f691e6aebe284ebe8d
SatvikGadamsetty <gadamsetty32892@gmail.com>
2014-08-10 13:05:08 -0400

    First commit
    Plots and code files for project 1.

---
+++ plot2.r
@@ -0,0 +1,18 @@
+## ## Reading in full data set from home directory
+## Converting date to appropriate format %d-%m-%Y
+full_data <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+full_data$Date <- as.Date(full_data$Date, format="%d/%m/%Y")
+
+## Subsetting the data for data of interest: between Feb 01 and Feb 02
+data <- subset(full_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+## Converting dates to appropriate format %d-%m-%Y %h-%m-%s
+full_date <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(full_date)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type = "l",ylab="Global Active Power (kilowatts)", xlab = "")
+
+## Saving to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
